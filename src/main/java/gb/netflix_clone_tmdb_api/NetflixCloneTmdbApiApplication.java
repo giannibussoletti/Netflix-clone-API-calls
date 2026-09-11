bf06fc0ddeb302defab8a11527b93583f3175c80
giannibussoletti <bussolettigianni@gmail.com>
2026-09-11 18:39:25 +0200

    initial commit

---
+++ src/main/java/gb/netflix_clone_tmdb_api/NetflixCloneTmdbApiApplication.java
@@ -0,0 +1,13 @@
+package gb.netflix_clone_tmdb_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetflixCloneTmdbApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetflixCloneTmdbApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gb/netflix_clone_tmdb_api/NetflixCloneTmdbApiApplicationTests.java
@@ -0,0 +1,13 @@
+package gb.netflix_clone_tmdb_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixCloneTmdbApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
